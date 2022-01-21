b895c11cb5c1033a2188f48b46cadbd1d57bf603
Dominio <DDominio@hollandamericagroup.com>
2022-01-21 17:58:25 +0100

    Final changes to the JS code

--- mixmess.js
+++ mixmess.js
@@ -1,9 +1,9 @@
 //an object made of array with the string of the sentences?
 
 const mixedMessage = {
-    _begin : [],
-    _middle : [],
-    _final : [],
+    _begin : ['You' , 'Friend', 'Dude' , 'Amigo' , 'Buddy'],
+    _middle : ['have to' , 'need to' , 'should' , 'will' , 'eventually'],
+    _final : ['count your blessings!' , 'look after yourself.' , 'understand this later.' , 'think about it...' , 'go for it!'],
 
     get begin () {
         return this._begin;
@@ -16,6 +16,18 @@ const mixedMessage = {
     get final () {
         return this._final;
     }
+
+    
 }
 
-console.log('something');
+function generateRandom (arr) {
+    return arr[Math.floor(Math.random() * 5)];
+
+}
+
+console.log(`${generateRandom(mixedMessage.begin)} ${generateRandom(mixedMessage.middle)} ${generateRandom(mixedMessage.final)}`);
+
+
+
+
+
