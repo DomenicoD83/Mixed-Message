f5c16b39a013b21a9e6542dc10bd2044af7cf927
Dominio <DDominio@hollandamericagroup.com>
2022-01-21 16:47:15 +0100

    first commit, started off the README file and begin the script. Populated the arrays of strings.

---
+++ mixmess.js
@@ -0,0 +1,21 @@
+//an object made of array with the string of the sentences?
+
+const mixedMessage = {
+    _begin : [],
+    _middle : [],
+    _final : [],
+
+    get begin () {
+        return this._begin;
+    },
+
+    get middle () {
+        return this._middle;
+    },
+
+    get final () {
+        return this._final;
+    }
+}
+
+console.log('something');
